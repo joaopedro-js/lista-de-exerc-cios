79abde098efe73b8a8276d26b13cf9af4dc332fc
João pedro <joaopedroribeiromonteiro@gmail.com>
2025-08-26 23:32:39 -0300

    adicionar alterações no arquivo 01.js

--- 01.js
+++ 01.js
@@ -1,3 +1,5 @@
+//1) O proprietário da empresa ABC LTDA precisa de um programa de computador para calcular o novo salário que seus funcionários irão receber a partir do mês que vem. Sabendo que o aumento de salário para todos os funcionários será de 25%, faça um programa que lê o valor do salário atual do funcionário e informa o seu novo salário acrescido de 25%. 
+
 var salary = prompt("Enter your salary");
 new_salary = salary * 1.25;
 console.log(new_salary)
